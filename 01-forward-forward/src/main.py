27717f9570718cc802438b5c113d58b661be5a3c
Andrew Smith <andrew.smith.recruiting@gmail.com>
2023-03-08 15:52:59 -0800

    forward forward in src/ and cleanup

--- 01-forward-forward/src/main.py
+++ 01-forward-forward/src/main.py
@@ -1,37 +1,35 @@
+"""
+Implementation of this paper:
+https://arxiv.org/pdf/2212.13345.pdf
+
+The below implementation was retrieved from this repo, and modified to run on MPS:
+https://github.com/mohammadpz/pytorch_forward_forward/blob/main/main.py
+
+If we want to expand on this, good next step is to:
+1. Implement the masking from the paper
+"""
+
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+from torch.optim import Adam
+from torchvision.datasets import MNIST
 from torchvision.transforms import Compose, ToTensor, Normalize, Lambda
 from torch.utils.data import DataLoader
-from torchvision.datasets import MNIST
-import torch
 
-"""
-Criticism of other FF impl to keep in mind:
-
-The predict method computes the "goodness" score for each possible label by
-iterating over all 10 labels and computing the score for each label separately.
-This approach can be very slow and inefficient, especially for large datasets or
-complex models. A more efficient approach would be to compute the scores for all
-labels simultaneously using matrix multiplication.
-
-The train method uses a fixed threshold of 2.0 for the "goodness" scores of the
-positive and negative examples. This threshold may not be optimal for all
-datasets or models, and may lead to suboptimal performance. A better approach
-would be to use a dynamic threshold that adapts to the distribution of the
-"goodness" scores during training.
-
-The train method does not use any regularization techniques, such as weight
-decay or dropout, to prevent overfitting. This may lead to overfitting of the
-model to the training data, especially if the number of epochs is high.
-
- The train method does not use any validation or test sets to monitor the
- performance of the model during training. This may make it difficult to
- determine when the model has converged or whether it is overfitting.
-"""
+# this ensures that the current MacOS version is at least 12.3+
+print(torch.backends.mps.is_available())
+# this ensures that the current current PyTorch installation was built with MPS activated.
+print(torch.backends.mps.is_built())
+# set the output to device: mps
+device = torch.device("mps")
 
 def MNIST_loaders(train_batch_size=50000, test_batch_size=10000):
 
     transform = Compose([
         ToTensor(),
-        Normalize((0.1307,), (0.3081,)),
+        Normalize((0.1307,), (0.3081,)), # This is well known for MNIST, so just use it.
         Lambda(lambda x: torch.flatten(x))])
 
     train_loader = DataLoader(
@@ -48,5 +46,119 @@ def MNIST_loaders(train_batch_size=50000, test_batch_size=10000):
 
     return train_loader, test_loader
 
+
+def overlay_y_on_x(x, y):
+    """Replace the first 10 pixels of data [x] with one-hot-encoded label [y]
+    """
+    x_ = x.clone()
+    x_[:, :10] *= 0.0
+    x_[range(x.shape[0]), y] = x.max()
+    return x_
+
+
+class Net(torch.nn.Module):
+
+    def __init__(self, dims):
+        super().__init__()
+        self.layers = []
+        for d in range(len(dims) - 1):
+            self.layers += [Layer(dims[d], dims[d + 1]).to(device)]
+
+    def predict(self, x):
+        goodness_per_label = []
+        for label in range(10):
+            # 50000 x 784
+            h = overlay_y_on_x(x, label)
+
+            # TODO: convert goodness list to tensors?
+            goodness = []
+            for layer in self.layers:
+                # 50000 x 500
+                h = layer(h)
+
+                # list of len 2 [50000 x 1]
+                goodness += [h.pow(2).mean(1)]
+
+            # list of len 1 [50000 x 1]
+            goodness_per_label += [sum(goodness).unsqueeze(1)]
+
+        goodness_per_label = torch.cat(goodness_per_label, 1)
+        # size: [batch_size, labels]
+        return goodness_per_label.argmax(1)
+
+    def train(self, x_pos, x_neg):
+        h_pos, h_neg = x_pos, x_neg
+        for i, layer in enumerate(self.layers):
+            print('training layer', i, '...')
+            h_pos, h_neg = layer.train(h_pos, h_neg)
+
+
+class Layer(nn.Linear):
+    def __init__(self, in_features, out_features,
+                 bias=True, device=None, dtype=None):
+        super().__init__(in_features, out_features, bias, device, dtype)
+        self.relu = torch.nn.ReLU()
+        self.opt = Adam(self.parameters(), lr=0.03)
+        self.threshold = 2.0
+        self.num_epochs = 1000
+
+    def forward(self, x):
+        x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-4)
+        return self.relu(
+            torch.mm(x_direction, self.weight.T) +
+            self.bias.unsqueeze(0))
+
+    def train(self, x_pos, x_neg):
+        # Question: Why do epochs here?
+        # Answer: layers are trained independently of other layers
+        for i in tqdm(range(self.num_epochs)):
+            g_pos = self.forward(x_pos).pow(2).mean(1)
+            g_neg = self.forward(x_neg).pow(2).mean(1)
+            # The following loss pushes pos (neg) samples to
+            # values larger (smaller) than the self.threshold.
+            loss = torch.log(1 + torch.exp(torch.cat([
+                -g_pos + self.threshold,
+                g_neg - self.threshold]))).mean()
+            self.opt.zero_grad()
+            # this backward just compute the derivative and hence
+            # is not considered backpropagation.
+            loss.backward()
+            self.opt.step()
+
+        # Question: why forward pass again
+        # Answer: If we are stepping optimizer it makes sense to use stepped optimizer
+        return self.forward(x_pos).detach(), self.forward(x_neg).detach()
+
+    
+def visualize_sample(data, name='', idx=0):
+    reshaped = data[idx].cpu().reshape(28, 28)
+    plt.figure(figsize = (4, 4))
+    plt.title(name)
+    # plt.imshow(reshaped, cmap="gray")
+    # plt.show()
+    plt.savefig("results/" + name + '.png')
+    
+    
 if __name__ == "__main__":
-    pass
+    torch.manual_seed(1234)
+    train_loader, test_loader = MNIST_loaders()
+
+    # TODO: implement masking from paper
+    net = Net([784, 500, 500])
+    x, y = next(iter(train_loader))
+    x, y = x.to(device), y.to(device)
+    x_pos = overlay_y_on_x(x, y)
+    rnd = torch.randperm(x.size(0))
+    x_neg = overlay_y_on_x(x, y[rnd])
+    
+    for data, name in zip([x, x_pos, x_neg], ['orig', 'pos', 'neg']):
+        visualize_sample(data, name)
+    
+    net.train(x_pos, x_neg)
+
+    print('train error:', 1.0 - net.predict(x).eq(y).float().mean().item())
+
+    x_te, y_te = next(iter(test_loader))
+    x_te, y_te = x_te.to(device), y_te.to(device)
+
+    print('test error:', 1.0 - net.predict(x_te).eq(y_te).float().mean().item())
