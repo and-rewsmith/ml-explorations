693bd069d86aa3d2a4a7e3390c3b98b4847324bf
Andrew Smith <andrew.smith.recruiting@gmail.com>
2023-03-12 18:18:55 -0700

    cleanup

--- 03-simple-snn-stdp/src/stdp_with_gd_mnist.py
+++ 03-simple-snn-stdp/src/stdp_with_gd_mnist.py
@@ -1,17 +1,17 @@
 """
 Implementation for MNIST with simple SNN.
 
-Uses STDP learning for first layers, and gd for last layer.
+Uses hybrid approach where first layers trained through STDP and last layers
+trained with SGD.
 """
 
-import numpy as np
+import torchvision
+
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.optim import SGD, Adam
 import torch.utils.data as data
-import torchvision
-from torchvision.transforms import Compose, ToTensor, Normalize, Lambda
+
 from tqdm import tqdm
 
 from spikingjelly.activation_based import learning, layer, neuron, functional, encoding, surrogate
@@ -19,11 +19,11 @@ from spikingjelly.activation_based import learning, layer, neuron, functional, e
 
 def MNIST_loaders(batch_size):
     # TODO: integrate transform
-    transform = Compose([
-        ToTensor(),
-        # This is well known for MNIST, so just use it.
-        Normalize((0.1307,), (0.3081,)),
-        Lambda(lambda x: torch.flatten(x))])
+    # transform = Compose([
+    #     ToTensor(),
+    #     # This is well known for MNIST, so just use it.
+    #     Normalize((0.1307,), (0.3081,)),
+    #     Lambda(lambda x: torch.flatten(x))])
 
     train_dataset = torchvision.datasets.MNIST(
         root="./data",
@@ -62,23 +62,22 @@ def f_weight(x):
     return torch.clamp(x, -1, 1.)
 
 
-# TODO: poisson encoder
-# TODO: multiple epochs each with multiple batches
+# TODO: figure out why conv2d layers get error "Runtime canonicalization must
+#       simplify reduction axes to minor 4 dimensions." on mps
 if __name__ == "__main__":
-    # torch.set_printoptions(threshold=torch.inf)
     torch.autograd.set_detect_anomaly(True)
 
-    # this ensures that the current MacOS version is at least 12.3+
+    # this ensures that the current MacOS version is at least 12.3+ and pytorch
+    # has mps
     assert torch.backends.mps.is_available()
-    # this ensures that the current current PyTorch installation was built with MPS activated.
     assert torch.backends.mps.is_built()
-    # set the output to device: mps
-    device = torch.device("mps")
+    # set the output to device
+    device = torch.device("cpu")
 
-    epochs = 2
-    T = 100
-    lr = 0.01
+    TIMESTEPS = 100
+    lr = 0.1
     batch_size = 64
+    num_epochs = 1
 
     # if this changes we need to alter network input sizes in network and out
     step_mode = "m"
@@ -87,7 +86,7 @@ if __name__ == "__main__":
     tau_pre = 2.
     tau_post = 100.
 
-    # linear layers are the weights
+    # TODO: convert Conv2d layers to linear to simplify
     net = nn.Sequential(
         layer.Conv2d(1, 16, kernel_size=3, stride=1, padding=1, bias=False),
         neuron.IFNode(),
@@ -96,25 +95,25 @@ if __name__ == "__main__":
         neuron.IFNode(),
         layer.MaxPool2d(2, 2),
         layer.Flatten(),
-        layer.Linear(16 * 8 * 8, 64, bias=False),
+        layer.Linear(784, 64, bias=False),
         neuron.IFNode(),
         layer.Linear(64, 10, bias=False),
         neuron.IFNode(),
     ).to(device)
 
-    # train linear layer with stdp and ifnode with sgd
     functional.set_step_mode(net, step_mode)
+    net.train()
 
     # all but last 2 layers train with stdp
     stdp_learners = []
     instances_stdp = (layer.Conv2d, )
-    for i in range(0, net.__len__() - 2):
+    for i in range(0, net.__len__()):
         if isinstance(net[i], instances_stdp):
             stdp_learners.append(
                 learning.STDPLearner(step_mode=step_mode, synapse=net[i], sn=net[i+1], tau_pre=tau_pre, tau_post=tau_post,
                                      f_pre=f_weight, f_post=f_weight))
 
-    # last 2 layers train with sgd
+    # last 2 layers train with backprop
     params_stdp = []
     for m in net.modules():
         if isinstance(m, instances_stdp):
@@ -132,58 +131,68 @@ if __name__ == "__main__":
     optimizer_gd = Adam(params_gradient_descent, lr=lr)
     optimizer_stdp = SGD(params_stdp, lr=lr, momentum=0.)
 
-    # get data
     train_data_loader, test_data_loader, train_dataset, test_dataset = MNIST_loaders(
         batch_size)
 
-    net.train()
     encoder = encoding.PoissonEncoder()
     examples = enumerate(train_data_loader)
-    for batch_idx, (example_data, example_targets) in tqdm(enumerate(train_data_loader)):
-        print("training for batch: ", batch_idx)
-        x_seq = example_data.to(device)
-        example_targets = example_targets.to(device)
-        targets_onehot = torch.nn.functional.one_hot(example_targets).float()
+    for epoch in range(0, num_epochs):
+        print("starting training for epoch 1")
+        for batch_idx, (example_data, example_targets) in tqdm(enumerate(train_data_loader)):
+            print()
 
-        functional.reset_net(net)
+            functional.reset_net(net)
+            optimizer_stdp.zero_grad()
+            optimizer_gd.zero_grad()
 
-        # convert to sensible shape
-        x_seq = x_seq.view(x_seq.size(0), -1)
-        x_seq = torch.unsqueeze(x_seq, 0).repeat(T, 1, 1)
-        x_seq = x_seq.view(T, 64, 1, 28, 28)
-        x_seq = encoder(x_seq)
-        print("x: " + str(x_seq.shape))
+            print("training for batch: ", batch_idx)
+            x_seq = example_data.to(device)
+            example_targets = example_targets.to(device)
+            targets_onehot = torch.nn.functional.one_hot(
+                example_targets).float()
 
-        # TODO: figure this out! This is causing the failure
-        y = functional.multi_step_forward(x_seq.unsqueeze(0), net)
-        y = torch.mean(y, dim=0)
-        _, predicted = torch.max(y, dim=1)
-        print()
-        print()
+            # convert to sensible shape:
+            # [Time, batches, channel size, height, width]
+            # = [Time, T, C, H, W]
+            x_seq = x_seq.view(x_seq.size(0), -1)
+            x_seq = torch.unsqueeze(x_seq, 0).repeat(TIMESTEPS, 1, 1)
+            x_seq = x_seq.view(TIMESTEPS, 64, 1, 28, 28)
+            x_seq = encoder(x_seq)
 
-        loss_fn = nn.CrossEntropyLoss()
-        loss = loss_fn(y, targets_onehot)
+            # at this point we have tensor of shape: [Time, T, C, H, W]
+            # we need to reshape to add channel size: [Time, T, N, C, H, W]
+            print("x input: " + str(x_seq.shape))
+            Z, T, C, H, W = x_seq.shape
+            N = 1
+            x_seq = x_seq.unsqueeze(2)
+            x_seq.view(Z, T, N, C, H, W)
+            print("x reshaped: " + str(x_seq.shape))
 
-        print("loss: " + str(loss))
+            y = functional.multi_step_forward(x_seq, net)
+            y = torch.mean(y, dim=0)
+            y = y.squeeze(1)
+            y = torch.softmax(y, dim=1)
 
-        loss.backward()
+            print("target probabilities: ", y.shape)
 
-        # zero gradients for non backprop trained layers
-        optimizer_stdp.zero_grad()
+            loss_fn = nn.BCELoss()
+            loss = loss_fn(y, targets_onehot)
 
-        for i in range(stdp_learners.__len__()):
-            stdp_learners[i].step(on_grad=True)
+            print("loss: " + str(loss))
 
-        optimizer_gd.step()
-        optimizer_stdp.step()
+            loss.backward()
 
-        optimizer_stdp.zero_grad()
-        optimizer_gd.zero_grad()
+            # zero gradients for non sgd trained layers
+            optimizer_stdp.zero_grad()
 
-        print("params_gradient_descent: ")
-        for i, p in enumerate(params_gradient_descent):
-            if i == 0:
-                print(p[0][0:5])
-        print()
+            for i in range(stdp_learners.__len__()):
+                stdp_learners[i].step(on_grad=True)
 
-    print("done")
+            optimizer_gd.step()
+            optimizer_stdp.step()
+
+            # print params to make sure they get updated between batches
+            print("params_gradient_descent: ",
+                  params_gradient_descent[0][0][0:5])
+
+        print("finished training for epoch 1")
